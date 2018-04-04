554a79295b6913af3c3beeef85eb79a956e943f5
Vic Tirta <vtirta@gmail.com>
2018-04-03 21:50:55 -0700

    fixed private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import PrivateRoute from "./components/PrivateRoute";
+import Auth from './utils/Auth';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import Users from "./pages/Users";
@@ -9,6 +9,24 @@ import Nav from "./components/Nav";
 import Login from "./pages/Login";
 import Logout from "./components/Logout";
 import SignUp from "./pages/SignUp";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      Auth.isUserAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+    }
+  />
+);
 
 const App = () =>
   <Router>
